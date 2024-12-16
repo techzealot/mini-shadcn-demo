f3dd2bde20bc9ac955ff83e56f8860161562c817
techzealot <techzealot@foxmail.com>
2024-12-17 03:01:30 +0800

    使用dark文字暂时保留暗色主题变量避免dark mode失效

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import { Button } from "@/components/button";
 function App() {
   return (
     <>
-      <Button>Hello World</Button>
+      {/* 由于使用的是css variable切换明暗主题,由于没有切换功能,为避免tailwind去除.dark css需要在工程中出现dark文字以保留相关css */}
+      <Button>"dark" text to reserve dark css</Button>
     </>
   );
 }
