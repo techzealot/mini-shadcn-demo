c650d6177a4d6d1d95612417ed1a4e0610a068d6
techzealot <techzealot@foxmail.com>
2024-12-17 22:08:53 +0800

    补充button stories

--- src/components/button.stories.tsx
+++ src/components/button.stories.tsx
@@ -1,9 +1,28 @@
-import type { Meta, StoryObj } from "@storybook/react";
+import { PlusSquareIcon } from "lucide-react";
+
+import type {
+  Meta,
+  StoryObj,
+} from "@storybook/react";
+import { fn } from "@storybook/test";
 
 import { Button } from "./button";
 
 const meta = {
   component: Button,
+  argTypes: {
+    variant: {
+      options: ["default", "secondary"],
+      control: { type: "select" },
+    },
+    size: {
+      options: ["default", "sm", "lg", "icon"],
+      control: { type: "select" },
+    },
+  },
+  args: {
+    onClick: fn(),
+  },
 } satisfies Meta<typeof Button>;
 
 export default meta;
@@ -15,5 +34,20 @@ export const Default: Story = {
     variant: "default",
     size: "default",
     children: "Button",
+  },
+};
+export const Secondary: Story = {
+  args: {
+    variant: "secondary",
+    size: "sm",
+    children: "Button",
+  },
+};
+
+export const Icon: Story = {
+  args: {
+    variant: "default",
+    size: "icon",
+    children: <PlusSquareIcon />,
   },
 };
--- src/components/text.tsx
+++ src/components/text.tsx
@@ -6,6 +6,7 @@ import {
 import { cn } from "@/lib/utils";
 import { Slot } from "@radix-ui/react-slot";
 
+//ignore
 interface TextProps extends ComponentPropsWithRef<"div"> {
   asChild?: boolean;
 }
